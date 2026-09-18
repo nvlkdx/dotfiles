b5faf5ab0206f7da0f15c8794cdc162830c0c200
nyl <sydnyl@tutamail.com>
2026-09-18 16:51:04 +0000

    .

--- dot_config/rmpc/executable_change.sh
+++ dot_config/rmpc/executable_change.sh
@@ -5,4 +5,4 @@ source ~/.env/USR_MUSIC_DIR
 
 magick "$(shuf -n 1 -e $RMPC_DEFAULT_ART_DIR/*)" -gravity center -crop 1:1+0+0 +repage ~/.config/rmpc/default.png
 
-ffmpeg -loglevel quiet -y -i "$USR_MUSIC_DIR/$(mpc current -f %file%)" -an -c:v copy /tmp/cover.jpg && notify-send -t 2000 -u low -e -i /tmp/cover.jpg "$(mpc current)"
+ffmpeg -loglevel quiet -y -i "$USR_MUSIC_DIR/$(mpc current -f %file%)" -an -c:v copy /tmp/cover.jpg && notify-send -t 2000 -u low -e -i /tmp/cover.jpg "$(mpc current -f %title%)" "$(mpc current -f %artist%)"
